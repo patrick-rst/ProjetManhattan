f880c2f9859a2be0c28c94fd5677fb830d21e00e
patrick-rst <patrick-rst@users.noreply.github.com>
2015-03-25 10:50:46 -0400

    added id to imageview in fxml

--- src/ca/qc/bdeb/sim/projetmanhattan/Main.java
+++ src/ca/qc/bdeb/sim/projetmanhattan/Main.java
@@ -17,7 +17,6 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("view/FXMLDocument.fxml"));
         
-        
         Scene scene = new Scene(root);
         
         stage.setTitle("Simtronique");
