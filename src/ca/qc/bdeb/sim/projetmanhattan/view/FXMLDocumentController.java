0f7543d8b43af7c5b2761d9932fd64c223a2a705
patrick-rst <patrick-rst@users.noreply.github.com>
2015-03-29 11:43:55 -0400

    modification des valeurs dans l'ui (marche pas...)

--- src/ca/qc/bdeb/sim/projetmanhattan/view/FXMLDocumentController.java
+++ src/ca/qc/bdeb/sim/projetmanhattan/view/FXMLDocumentController.java
@@ -2,18 +2,28 @@ package ca.qc.bdeb.sim.projetmanhattan.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import org.controlsfx.control.PopOver;
+import org.controlsfx.control.PopOver.ArrowLocation;
 
 
 /**
@@ -122,9 +132,86 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void mouseClickCase(MouseEvent event) {
-        ImageView source = (ImageView) event.getSource(); 
-        source.setRotate(source.getRotate()+90);
-    }
+        ImageView source = (ImageView) event.getSource();
+        if (event.getButton().equals(MouseButton.PRIMARY)) { 
+            source.setRotate(source.getRotate()+90);            
+        } 
+        else if(event.getButton().equals(MouseButton.SECONDARY)) {
+            String id = source.getId();
+            
+            int row = grid.getRowIndex(source);
+            int column = grid.getColumnIndex(source);             
+            
+            Label lblComposant = new Label();
+            Label lblUnite = new Label();
+            TextField txtValeur = new TextField();
+            txtValeur.setPrefWidth(50);
+            Button btn = new Button("Ok");
+            btn.setId(row+","+column);
+            
+            if (id.equals("source")) {
+                lblComposant.setText("Source");
+                lblUnite.setText("Volt");
+            }
+            else if (id.equals("resistance")) {
+                
+            }
+            else if (id.equals("filDroit")) {
+                
+            }
+            else if (id.equals("filCoin")) {
+                
+            }
+            else if (id.equals("filT")) {
+                
+            }
+            else if (id.equals("filCroix")) {
+                
+            }
+            else {
+                System.out.println("ERROR:Composant not implemented");
+            }     
+ 
+            HBox box = new HBox();
+            box.setPadding(new Insets(15, 15, 15, 15));
+            box.setSpacing(10);
+            box.getChildren().addAll(lblComposant, txtValeur, lblUnite, btn);
+            
+            
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    Button source = (Button) event.getSource();
+                    String id = source.getId();
+                    
+                    int row = Integer.parseInt(id.split(",")[0]);
+                    int column = Integer.parseInt(id.split(",")[1]);
+                    
+                    String composant = circuit[row][column].toString();
+                    
+                    if (composant.equals("Resistance")) {
+                        ResistanceGraphique resistance = (ResistanceGraphique) circuit[row][column];
+                        resistance.setResistance(Double.parseDouble(txtValeur.getText()));
+                    } else if (composant.equals("SourceFEM")) {
+                        SourceFEMGraphique sourceFEM = (SourceFEMGraphique) circuit[row][column];
+                        sourceFEM.setForceElectroMotrice(Double.parseDouble(txtValeur.getText()));
+                    }
+                    
+                }
+            });
+            
+            
+            PopOver composantEditor = new PopOver(box);
+            //composantEditor.setContentNode(box);
+            //composantEditor.setArrowLocation(ArrowLocation.BOTTOM_LEFT);
+            //composantEditor.setCornerRadius(4);
+            //composantEditor.setDetachedTitle("Composant");
+            composantEditor.show((ImageView) event.getSource(), 15);           
+            
+            
+            
+        }
+    }     
     
     private void addComposant(String id, int row, int column) {
         if (id.equals("source")) {
