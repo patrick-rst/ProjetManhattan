8de913983b39207cfa54f2c7e358c8fa9c6b2d14
patrick-rst <patrick-rst@users.noreply.github.com>
2015-05-21 09:09:30 -0400

    k

--- src/ca/qc/bdeb/sim/projetmanhattan/Main.java
+++ src/ca/qc/bdeb/sim/projetmanhattan/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Simtronique");
+        stage.setTitle("Projet Manhattan");
         CircuitAnalogue circuitAnalogue = new CircuitAnalogue();
         CircuitDigital circuitNumerique = new CircuitDigital();
         AnalyseC circuitGraphique = new AnalyseC();
